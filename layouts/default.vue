53071e449a286dab92da5ab6c0c9a44f0ecdada6
NickCrawford <hi@nickcrawford.design>
2020-03-30 16:14:58 -0400

    Initial Commit and Styling

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+     <b-navbar type="is-primary">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="@/assets/images/galileo-logotype.svg"
+                    alt="Galileo Health"
+                >
+            </b-navbar-item>
+        </template>
+     </b-navbar>
+
+     <nuxt />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <!-- Page Heading & Controls-->
+    <section class="section is-small">
+      <div class="level">
+        <div class="level-left">
+          <h1 class="level-item title is-3">Provider Task List</h1>
+        </div>
+        <div class="level-right">
+          <b-button class="level-item" type="is-info" rounded icon-right="plus"
+            >Add Provider To View</b-button
+          >
+        </div>
+      </div>
+    </section>
+
+    <!-- Providers Container -->
+    <main class="provider-list">
+      <div class="card"></div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProvidersMasterView',
+
+  components: {}
+}
+</script>
